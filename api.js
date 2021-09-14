fbb3f2f680d6ceac83ba30b5a5c815e952ca56c6
Rahatutzaman rizon <rizonrahat199@gmail.com>
2021-09-14 23:41:20 +0600

    your message

--- api.js
+++ api.js
@@ -16,15 +16,16 @@ const showProducts = (products) => {
         </div>
         <h3>${product.title}</h3>
         <p>Category: ${product.category}</p>
-        <p>Ratting:  ${product.rating.rate}</p>
-        <p>Ratting:  ${product.rating.count}</p>
+        <p class="star"><i class="far fa-star"> </i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"> </i>${product.rating.rate}</p>
+        <p>People count:  ${product.rating.count}</p>
+    
         <h2>Price: $ ${product.price}</h2>
         <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
         <button id="details-btn" class="btn btn-danger">Details</button></div>
         `;
         document.getElementById("all-products").appendChild(div);
     }
-    updateTotal();
+    updateTotal();//call function
 };
 let count = 0;
 const addToCart = (id, price) => {
